6d37cd2eb6f43c48b97f742cb0d715bab39ca138
Charissa Beaty <char.beaty11@gmail.com>
2020-01-21 09:42:38 -0700

    Added routes and mongodb to application

--- google-books/src/App.js
+++ google-books/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar"
 import Jumbotron from "./components/Jumbotron"
+import Books from "./pages/Books"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Jumbotron />
+      <Books />
     </div>
   );
 }
--- google-books/src/components/Navbar.js
+++ google-books/src/components/Navbar.js
@@ -2,14 +2,11 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg ">
       <a className="navbar-brand" href="/">
         Google Books
       </a>
-      <a className="navbar-brand" href="/">
-        Search
-      </a>
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand" href="/saved">
         Saved
       </a>
     </nav>
